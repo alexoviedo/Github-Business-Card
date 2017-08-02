f5a89392f1845a070e36b6d6a3ef3b8c6e29646b
alexoviedo <oviedo.alex@gmail.com>
2017-08-02 14:33:59 -0400

    Github business card API project. First commit.

---
+++ main.js
@@ -0,0 +1,43 @@
+// ========LOOP FOR CREATING ALL 'person' elements on page========
+
+for (var i = 0; i < customers.results.length; i += 1) {
+
+  // =========PULLS HTML INTO JS ASSIGNS IT TO JS VARIABLE customerID======
+  let customerID = document.querySelector("#customer")
+
+  // ======CREATES A DIV INSIDE OF HTML. ALL OTHER CONTAINERS WILL BE INSIDE THIS DIV=====
+  let customerChild = document.createElement('div');
+
+  // ====='person' REPRESENTS ALL INNERHTML======
+
+  var person = `
+    <div class= "bodyMargins">
+    <div class= "pic"
+    style="
+    background: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 250px;
+    margin: 10px;
+    background-image: url(${customers.results[i].picture.large});
+    "></div>
+
+    <h2 style="text-transform: capitalize;
+      font-size: 20px;
+      text-decoration: underline solid green;">
+      ${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
+
+    <h3 style="
+    font-weight: lighter;">${customers.results[i].email}</h3>
+
+    <div style="line-height: 40%;  margin: 15% auto;">
+    <p>${customers.results[i].location.street}</p>
+    <p>${customers.results[i].location.city}, ${customers.results[i].location.state} ${customers.results[i].location.postcode} </p>
+    <p>${customers.results[i].phone}</p>
+    </div>
+    </div>`
+
+  // ==========END OF HTML INNERHTML BLOCK==============
+  customerID.appendChild(customerChild);
+  customerChild.innerHTML = person;
+}
